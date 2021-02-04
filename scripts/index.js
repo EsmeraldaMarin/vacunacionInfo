3a6056017fe306e64c57b398e5607e5f9580a268
Esmeralda Marin <esmemarinm03@gmail.com>
2021-02-04 20:28:25 -0300

    correciones header

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,26 @@
 let arrowDown = document.querySelectorAll('.arrowDown')
-arrowDown.forEach(arrow=>{
-    arrow.addEventListener('click', (e)=>{
-        let parent =  e.target.parentNode;
-        parent.classList.toggle('cardActive')
+if(arrowDown){
+    arrowDown.forEach(arrow=>{
+        arrow.addEventListener('click', (e)=>{
+            let parent =  e.target.parentNode;
+            parent.classList.toggle('cardActive')
+        })
     })
+}
+
+//menu hamb
+
+let btnMenu = document.getElementById('iconosMenu');
+let bars = document.getElementById('bars');
+let cruz = document.getElementById('cruz');
+let navegador = document.getElementById('navegador')
+btnMenu.addEventListener('click', ()=>{
+    navegador.classList.toggle('menuActive')
+    if(navegador.classList.contains('menuActive')){
+        cruz.classList.add('iconActive');
+        bars.classList.remove('iconActive');
+    }else{
+        bars.classList.add('iconActive');
+        cruz.classList.remove('iconActive');
+    }
 })
